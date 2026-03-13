fc914e5d862323f5b3ae79f7ba907ed5904b751b
Auohhjcgjfhfgseertresdffgyyuiyt <125882163+Auohhjcgjfhfgseertresdffgyyuiyt@users.noreply.github.com>
2026-03-13 17:57:14 +0700

    backup src/settings.js

---
+++ src/settings.js
@@ -0,0 +1,51 @@
+//------------ OWNER SETTINGS
+global.nomerOwner = "6288215204772";
+global.nomerOwner2 = "6285701414272";
+global.ownerPhoto = ""; // photo owner
+global.ownerName = "TIRO";
+global.gender = "Boys";
+global.agama = "Islam";
+global.tanggalLahir = "17/12/1998"; // contoh '25/11/1993'
+global.hobi = "ngoding";
+global.sifat = "tukang satir";
+global.tempatTinggal = "Magelang-Jogja";
+global.waifu = "Megawati";
+
+//------------ BOT SETTINGS
+//global.nomerBot = "6285701414272"
+global.nomerBot = "6287782401679"//untuk pairing code
+global.pairingCode = true; // true / false
+global.botName = "Violet";
+global.nickName = "Vio";
+global.session = "session";
+global.runWith = "DIgital Ocean 🐋";
+global.antiSpam = true;
+global.fake = botName;
+global.print = true; //menampilkan pesan di log
+global.copyright = `© ${botName}`;
+global.packName = "Violet Extension";
+global.authorName = "";
+global.ownerBot = `${nomerOwner}@s.whatsapp.net`;
+global.limitCount = 15;
+global.gameCount = 30;
+global.Intervalmsg = 2000; //detik
+global.cooldownTime = 30_000;
+global.myUrl = "https://wa.me/c/6285953938243";
+global.newsletterJid = "120363249625006239@newsletter";
+global.newsletterName = "VIOLET INFO";
+global.botPhoto = "https://telegra.ph/file/a3b739eb2b77785c930b9.jpg"
+
+//------------ Photo Bot SETTINGS
+global.fotoRandom = ["https://telegra.ph/file/63e8a49b86824c3402c2a.jpg"];
+
+//------------ MongoDB SETTINGS
+global.mongodb = "mongodb+srv://tiroodev:sJ1CykRInod30Ah3@botsore.hbjky.mongodb.net/?retryWrites=true&w=majority";
+global.dbName = "botstore";
+
+//------------ Apikeys SETTINGS
+global.apiflash = "d8290292a8d04396a8d870ffc8e12838"; //
+global.goFileApi = "MbdgCaYKJ27rNtallnRjqxLCOdEm8tIk";
+global.serApi = "510168b3708fdc9cb9954c7228446799e206745ec5d1154ad0a1a9b2c6244ca6";
+global.geminiApi = "AIzaSyCh3nquTfRQs2juL2SddgXd5bC37pWT1jQ"; //Dapatkan apikey di https://aistudio.google.com/api-keys, untuk bisa mengaktifkan ai
+global.elevenlabsApi = "sk_f144d536953d69153f9df76ab0185b3e7de044bb8fd8ec04";
+global.geminiApiKeys = ['AIzaSyCh3nquTfRQs2juL2SddgXd5bC37pWT1jQ','AIzaSyC3i6qSI4lnIMcsTIttsqKskuKUxT9jCPM'];
